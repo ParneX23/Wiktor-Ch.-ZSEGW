6a7e58e2269ce2aa2993e60ad7cf74b21e9df069
ParneX23 <144279846+ParneX23@users.noreply.github.com>
2025-04-08 17:19:28 +0200

    Delete ApiPogoda2/ApiPogoda2 directory

--- ApiPogoda2/ApiPogoda2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.apipogoda2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-    opens com.example.apipogoda2 to javafx.fxml;
-    exports com.example.apipogoda2;
-}
